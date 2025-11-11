a8d93d243020d38adf0ecadc809620fcec304f3e
franout <francesco.angione@polito.it>
2025-11-11 17:35:37 +0100

    feat: adding wally config for fsim flow and variable-based paths

--- zoix/netlist.f
+++ zoix/netlist.f
@@ -1,2 +1,3 @@
-../../gate/wallypipelinedcore.sv
-../strobe_cvw.sv
+${WALLY}/gate/${WALLY_CONFIG}/wallypipelinedcore.sv
+${WALLY}/zoix/strobe_cvw.sv
+-v ${WALLY}/gate/${WALLY_CONFIG}/techLib.v
--- zoix/zoix_cvw.sh
+++ zoix/zoix_cvw.sh
@@ -1,21 +1,27 @@
+#!/bin/bash
 
 rm -r ./run_zoix
 mkdir ./run_zoix
 cd ./run_zoix
 
+
+if [[ $# -ne 1 ]]; then 
+echo "Please provide a Wally configuration"
+exit 1
+fi 
+
+export WALLY_CONFIG=$1
 # Compile DUT & strobe file
 #
 
-zoix -f ../netlist.f  -l ../log.txt \
--v /data/libraries/NangateOpenCellLibrary_15_45_nm/45nm/NangateOpenCellLibrary.v \
-+timescale+override+1ns/1ps \
+zoix -f ../netlist.f  +timescale+override+1ns/1ps \
 +top+wallypipelinedcore_gate+strobe \
 +sv +notimingchecks +define+ZOIX +define+TOPLEVEL=wallypipelinedcore_gate +suppress+cell +delay_mode_fault +verbose+undriven -l zoix_compile.log
 
 
 #2 step, simulation:
-# addstrobe -fsgroup ../../sim/questa/core.vcd strobbed_vcd.vcd ../strobe.strobe 21000 10000
-./zoix.sim +vcd+file+"../../sim/questa/core.vcd" \
+
+./zoix.sim +vcd+file+"${WALLY}/sim/questa/core_gate.vcd" \
  +vcd+dut+wallypipelinedcore_gate+testbench.dut.core_gate +vcd+verify +vcd+verbose  -l logic_sim.log +vcd+limit+mismatch+100000
 
 
